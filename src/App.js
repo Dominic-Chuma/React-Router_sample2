c8b2318ec4c2b095f5174d41d4d58aff5fa37052
Dominic Chuma <chumadominic@gmail.com>
2022-08-30 21:37:42 +0100

    Adaptations made to use react-bootstrap powered Navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+// Import Router & other pages that are to be routed to
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './pages/Layout';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout/>}> 
+          
+            {/* <Route index element={<Home/>} /> 
+            <Route path='blogs' element={<Blogs/>} /> 
+            <Route path='contact' element={<Contact/>} />
+            <Route path='*' element={<NoPage/>} /> */}
+            
+           
+          {/* </Route> */}
+          {/* Nest another set of routes*/}
+          {/* <Route path='products' element={<Info/>} > */}
+            
+          </Route>
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
