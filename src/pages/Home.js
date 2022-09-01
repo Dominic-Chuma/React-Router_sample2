1b4e3c73c7d1030ef41ded832a0af3d31e8cb260
Dominic Chuma <chumadominic@gmail.com>
2022-09-01 22:30:17 +0100

    React-icons installed and work started on the Info-Icon Div

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,16 @@
 import MyCarousel from "./Sub-components/Carousels/Carousel";
 import DemarcationDiv from "./Sub-components/Divs/demarcation";
+import InfoWithIconDiv from "./Sub-components/Divs/info-icons";
 
 const Home = () => {
     return (
         // Commence on the Carousel tomorrow....
         <>
+            {/* Home Page - Beginning.... */}
             <MyCarousel />
             <DemarcationDiv/>
+            <InfoWithIconDiv/>
+            {/* Home Page - Ending... */}
         </>
     )
 }
