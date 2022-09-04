a1d2097eea2fecabd38f39cc72a173523a1cd455
Dominic Chuma <chumadominic@gmail.com>
2022-09-04 23:02:38 +0100

    Add itions were made to the InfoWithIconDiv

--- src/pages/Sub-components/Divs/info-icons.js
+++ src/pages/Sub-components/Divs/info-icons.js
@@ -1,4 +1,4 @@
-import { FaShoppingCart, FaCartArrowDown, FaGamepad, FaUserFriends, FaBook, FaBookReader, FaBookOpen, FaNetworkWired } from "react-icons/fa";
+import { FaCartArrowDown, FaGamepad, FaUserFriends, FaSuitcaseRolling, FaHandshake, FaLaptopCode } from "react-icons/fa";
 import Container from "react-bootstrap/Container";
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
@@ -76,8 +76,48 @@ const InfoWithIconDiv = () => {
                     <Card border="light" >
                         <Card.Body>
                             {/* <Card.Title>Card Title</Card.Title> */}
-                            <Card.Title> <FaBookReader className="font-icons" /> </Card.Title>
+                            <Card.Title> <FaLaptopCode className="font-icons" /> </Card.Title>
                             <Card.Subtitle className="mb-2 text-muted">Learning..</Card.Subtitle>
+                            <Card.Text>
+                            Some quick example text to build on the card title and make up the
+                            bulk of the card's content.
+                            </Card.Text>
+                            <Card.Link href="#">Card Link</Card.Link>
+                            <Card.Link href="#">Another Link</Card.Link>
+                        </Card.Body>
+                    </Card>
+                </Col>
+
+                {/* Column 4. */}
+                <Col sm='6' md='4' lg='4'>
+                    {/* <div className='info-icon-div'> */}
+                    {/* Some Things can be placed here llike welcome messages. */}
+                    {/* </div>  */}
+                    <Card border="light" >
+                        <Card.Body>
+                            {/* <Card.Title>Card Title</Card.Title> */}
+                            <Card.Title> <FaHandshake className="font-icons" /> </Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted">Hiring & Recruitment</Card.Subtitle>
+                            <Card.Text>
+                            Some quick example text to build on the card title and make up the
+                            bulk of the card's content.
+                            </Card.Text>
+                            <Card.Link href="#">Card Link</Card.Link>
+                            <Card.Link href="#">Another Link</Card.Link>
+                        </Card.Body>
+                    </Card>
+                </Col>
+
+                {/* Column 4. */}
+                <Col sm='6' md='4' lg='4'>
+                    {/* <div className='info-icon-div'> */}
+                    {/* Some Things can be placed here llike welcome messages. */}
+                    {/* </div>  */}
+                    <Card border="light" >
+                        <Card.Body>
+                            {/* <Card.Title>Card Title</Card.Title> */}
+                            <Card.Title> <FaSuitcaseRolling className="font-icons" /> </Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted">Travel</Card.Subtitle>
                             <Card.Text>
                             Some quick example text to build on the card title and make up the
                             bulk of the card's content.
