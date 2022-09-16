9da9def7d3a0584e33e5104883d6d425f1f4df78
Dominic Chuma <chumadominic@gmail.com>
2022-09-16 21:38:25 +0100

    The footer's 3 major column sections have been created.

--- src/pages/layout/footer.js
+++ src/pages/layout/footer.js
@@ -1,7 +1,37 @@
+import Container from "react-bootstrap/Container";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+
 const FooterDiv = () => {
     return (
         <div className='footer-div'>
-        {/* Some Things can be placed here llike welcome messages. */}
+        {/* Some Things can be placed here like welcome messages. */}
+            <Container className="footer-container">
+                <Row>
+                    <Col sm='6' md='4' lg='4'>
+                        <div>
+                            <h2> Net-Zone</h2>
+                        </div>
+                    </Col>
+                    <Col sm='6' md='4' lg='4'>
+                        <div>
+                            <h2> Net-Zone</h2>
+                        </div>
+                    </Col>
+                    <Col sm='12' md='4' lg='4'>
+                        <div>
+                            <h2> Net-Zone</h2>
+                        </div>
+                    </Col>
+                </Row>
+                <Row>
+                    <div>
+                        &copy; Energy Ransoms Limited.
+                    </div>
+                </Row>
+            </Container>
+
         </div>
         
     )
